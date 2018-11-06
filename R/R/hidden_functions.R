4783a40d76927c5202a3f1af25db640835cbda88
Hillary Koch <hillary.koch01@gmail.com>
2018-11-06 16:32:32 -0500

    fixed get_pseudo

--- R/R/hidden_functions.R
+++ R/R/hidden_functions.R
@@ -1,27 +1,25 @@
 # calculate pseudo variables for general GMCM
 get_pseudo <- function(u, mu, sigma, prop, k) {
-  # u: a vector of rescaled empirical marginal distribution function value
-  # mu: a vector of means for a given replicate, 1 for each component
-  # sigma: a vector of standard deviations for a given replicate, 1 for each component
-  # prop: proportion of each component in mixture model
-
+    # u: a vector of rescaled empirical marginal distribution function value
+    # mu: a vector of means for a given replicate, 1 for each component
+    # sigma: a vector of standard deviations for a given replicate, 1 for each component
+    # prop: proportion of each component in mixture model
+    
     # Compute cdf of gaussian mixture marginally for given replicate
     N <- 2000 # precision of grid for linear interpolation
-    # grid <-
-    #     seq(min(-4.5,-mu - 4.5 * sigma),
-    #         max(4.5, mu + 4.5 * sigma),
-    #         length.out = N)
     grid <-
-        seq(min(u) - min(u) / 2, max(u) + ((1 - max(u)) / 2), length.out = N)
-
+        seq(min(-4.5, -mu - 4.5 * sigma),
+            max(4.5, mu + 4.5 * sigma),
+            length.out = N)
+    
     G <-
         Reduce('+', lapply(seq(k), function(X)
             prop[X] * pnorm(grid, mu[X], sqrt(sigma[X]))))
-
+    
     # Do linear interpolation
     interp <- approx(x = G, y = grid, xout = u)
-
-  interp$y
+    
+    interp$y
 }
 
 # Calculate variance covariance matrix for constrained GMCM
@@ -35,7 +33,7 @@ get_constr_sigma <- function(Sigma, rho, idx) {
             }
         }
     }
-  Sigma
+    Sigma
 }
 
 # Calculate variance covariance matrix for constrained0 GMCM
