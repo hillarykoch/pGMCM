f09ecd11a1bb9ff7c47248bd713b9e71670ad6f9
Hillary Koch <hillary.koch01@gmail.com>
2018-11-28 18:17:15 -0500

    remove excessive prep.R file

--- R/R/prep.R
+++
@@ -1,8 +0,0 @@
-# Transform data based on quantile function of standard normal
-prep_dat <- function(data, n, type = c("PIT", "scale")) {
-    if(type == "scale") {
-        apply(data, 2, scale)    
-    } else {
-        apply(data, 2, function(X) qnorm(rank(X)/(n+1)))    
-    }
-}
